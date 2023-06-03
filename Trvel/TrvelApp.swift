362570c0073f4a0f77e33a7b69829836779c2e26
Mohamad Dzul Syakimin <dzulsyakimin61@gmail.com>
2023-06-03 20:23:41 +0800

    Initial Commit

---
+++ Trvel/TrvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrvelApp.swift
+//  Trvel
+//
+//  Created by Mohamad Dzul Syakimin on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
